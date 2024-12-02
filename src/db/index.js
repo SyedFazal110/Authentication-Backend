b1fd2058c318d5e9b0044109f2cb8f7dde72bedb
SyedFazal110 <syedfazal808@gmail.com>
2024-12-02 10:42:34 +0500

    Pending

--- src/db/index.js
+++ src/db/index.js
@@ -19,3 +19,19 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+const loginUser = async (req , res) => {
+  const { email , password } = req.body;
+
+  if(!email) return res.status(400).json({ message : "Email required"})
+  if(!password) return res.status(400).json({ message : "Password required"})
+  
+  // email ai ya nhe check kro
+
+  const user = await User.findOne({ email });
+  if(!user) return res.status(404).json({ message : "no user Found"})
+}
+
+
+export default connectDB
